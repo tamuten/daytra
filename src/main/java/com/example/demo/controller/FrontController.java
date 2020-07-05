2dbf4fc42426cdb1e25309a93e36da75bb1ba115
tamura <tamuten310@gmail.com>
2020-07-05 21:27:13 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/FrontController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.form.IndexForm;
+
+@Controller
+public class FrontController {
+	@RequestMapping({ "/", "/index" })
+	public String index(@ModelAttribute("indexForm") IndexForm form) {
+		return "index";
+	}
+
+	@RequestMapping("/regist")
+	public String regist(@ModelAttribute("indexForm") @Validated IndexForm form, BindingResult result) {
+
+		System.out.println("regist");
+
+		return "index";
+	}
+}
---
+++ src/main/java/com/example/demo/form/IndexForm.java
@@ -0,0 +1,14 @@
+package com.example.demo.form;
+
+public class IndexForm {
+	private String mailAddress;
+
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+}
